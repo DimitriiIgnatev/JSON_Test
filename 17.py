5e4ad894846f97ebec8c080e476e75f9554a261d
ignat99 <ignat99@gmail.com>
2018-11-26 00:47:09 +0300

    Jenkins 5

--- 17.py
+++ 17.py
@@ -256,7 +256,7 @@ def test_server_login(case, Server):
 #    assert_that(demo_token, has_length(20))
     assert_that(demo_token)
 
-data = {'orderRef':134, 'marketDirection': 'buy', 'currency': 'EUR', 'amount': '134.00', 'counterCurrency': 'USD','beneficiaryAccountRef':'BA-MVBDZBL3Z', 'paymentPurpose': 'services', 'valueDate': '30/11/2018'}
+data = {'orderRef':135, 'marketDirection': 'buy', 'currency': 'EUR', 'amount': '135.00', 'counterCurrency': 'USD','beneficiaryAccountRef':'BA-MVBDZBL3Z', 'paymentPurpose': 'services', 'valueDate': '30/11/2018'}
 
 #@idparametrize('case', [testclazz(login)
 #                                  for login in [my_token()]
