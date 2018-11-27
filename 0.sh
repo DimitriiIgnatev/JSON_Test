c264a3f7dac6730e3ba1e4b65d521f60b012e390
ignat99 <ignat99@gmail.com>
2018-11-27 04:32:02 +0300

    Sonar 2

--- 0.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -X POST --data-binary '{"jsonrpc":"2.0","id":"curltext","method":"get-user-profile","params":{"email":"", "auth_token":""}}' -H 'content-type:text/plain;' https://demo.*.com/api/login
-
